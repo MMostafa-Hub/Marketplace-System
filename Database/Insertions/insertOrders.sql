3c293a07df780f66497ff7533bfea8e922c18466
David Wagih <78867146+david-wagih@users.noreply.github.com>
2022-05-24 16:38:10 +0200

    Create insertOrders.sql

---
+++ Database/Insertions/insertOrders.sql
@@ -0,0 +1,11 @@
+INSERT INTO orders(user_username , dateCreated)
+VALUES ('David00', '2022-05-24 16:02:00');
+
+INSERT INTO orders(user_username , dateCreated)
+VALUES ('Mohanad00', '2022-05-24 16:02:00');
+
+INSERT INTO orders(user_username , dateCreated)
+VALUES ('Mario00', '2022-05-24 16:02:00');
+
+INSERT INTO orders(user_username , dateCreated)
+VALUES ('Marina00', '2022-05-24 16:02:00');
