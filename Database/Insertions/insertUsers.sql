2aa9080e228fa06be18813daef97fea178526574
David Wagih <78867146+david-wagih@users.noreply.github.com>
2022-05-24 16:37:08 +0200

    Create insertUsers.sql

---
+++ Database/Insertions/insertUsers.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (username, password, balance)
+VALUES ('David00', 'david123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Mohanad00', 'mohanad123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Marina00', 'marina123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Mario00', 'mario123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Mekky00', 'Mekky123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Maria00', 'maria123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Mostafa00', 'mostafa123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Nazeeh00', 'nazeeh123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Mahmoud00', 'mahmoud123', 5000);
+
+INSERT INTO users (username, password, balance)
+VALUES ('Hazem00', 'hazem123', 5000);
