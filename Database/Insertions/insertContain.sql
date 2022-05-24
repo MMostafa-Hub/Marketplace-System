2b587a2a5cd3e2b1ee8cfd4329659207a969997c
David Wagih <78867146+david-wagih@users.noreply.github.com>
2022-05-24 16:38:38 +0200

    Create insertContain.sql

---
+++ Database/Insertions/insertContain.sql
@@ -0,0 +1,26 @@
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (1 , 1 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (1 , 3 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (1 , 4 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (2 , 2 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (2 , 3 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (2 , 4 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (3 , 2 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (3 , 3 , 1);
+
+INSERT INTO contain(order_no ,product_id , quantity)
+VALUES (3 , 4 , 1);
