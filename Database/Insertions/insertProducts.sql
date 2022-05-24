814e030b74b63aaa48b45f503474844e76b37e40
David Wagih <78867146+david-wagih@users.noreply.github.com>
2022-05-24 16:37:45 +0200

    Create insertProducts.sql

---
+++ Database/Insertions/insertProducts.sql
@@ -0,0 +1,14 @@
+INSERT INTO Product(name, category, price , stockQuantity , soldQuantity)
+VALUES ('iPhone 13', 'Mobiles', 13000, 10 , 0);
+
+INSERT INTO Product(name, category, price , stockQuantity , soldQuantity)
+VALUES ('Samsung S12', 'Mobiles', 11000, 7 , 0);
+
+INSERT INTO Product(name, category, price , stockQuantity , soldQuantity)
+VALUES ('Mx Master 3', 'Mouses', 4000, 30 , 0);
+
+INSERT INTO Product(name, category, price , stockQuantity , soldQuantity)
+VALUES ('LG C2', 'TV', 25000, 5 , 0);
+
+INSERT INTO Product(name, category, price , stockQuantity , soldQuantity)
+VALUES ('MacBook Pro', 'Laptops', 40000, 8 , 0);
