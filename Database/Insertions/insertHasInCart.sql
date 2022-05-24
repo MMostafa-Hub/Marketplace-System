ade380aa9e973726f9c61db560f7412518aa9906
David Wagih <78867146+david-wagih@users.noreply.github.com>
2022-05-24 16:39:26 +0200

    Create insertHasInCart.sql

---
+++ Database/Insertions/insertHasInCart.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('David00' , 1 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('David00' , 3 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('David00' , 4 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('Mohanad00' , 2 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('Mohanad00' , 3 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('Mohanad00' , 4 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('Mario00' , 2 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('Mario00' , 3 , 1);
+
+INSERT INTO has_in_cart(user_username ,product_id , quantity)
+VALUES ('Mario00' , 4 , 1);
