f89bbec4cf279be0d858c168abc97c1bcd7d7b6b
Mohanad <mohanadismail789@gmail.com>
2022-06-19 14:54:21 +0200

    Add connection page load behavior

--- Client/Client App/Client App/ConnectionForm.Designer.cs
+++ Client/Client App/Client App/ConnectionForm.Designer.cs
@@ -31,6 +31,7 @@
             this.IPLabel = new System.Windows.Forms.Label();
             this.portLabel = new System.Windows.Forms.Label();
             this.connectButton = new System.Windows.Forms.Button();
+            this.instructionLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // IPLabel
@@ -59,17 +60,29 @@
             this.connectButton.TabIndex = 2;
             this.connectButton.Text = "button1";
             this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(266, 167);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(50, 20);
+            this.instructionLabel.TabIndex = 3;
+            this.instructionLabel.Text = "label1";
             // 
             // ConnectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.instructionLabel);
             this.Controls.Add(this.connectButton);
             this.Controls.Add(this.portLabel);
             this.Controls.Add(this.IPLabel);
             this.Name = "ConnectionForm";
             this.Text = "Connection Page";
+            this.Load += new System.EventHandler(this.ConnectionForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +93,6 @@
         private Label IPLabel;
         private Label portLabel;
         private Button connectButton;
+        private Label instructionLabel;
     }
 }
