92280fb96e17a5404a051c0b338bc5cf9dcb6efa
Mohanad <mohanadismail789@gmail.com>
2022-06-19 14:54:21 +0200

    Add connection page load behavior

--- Client/Client App/Client App/ConnectionForm.cs
+++ Client/Client App/Client App/ConnectionForm.cs
@@ -1,10 +1,29 @@
+using System.Configuration;
+using System.Collections.Specialized;
+
 namespace Client_App
 {
     public partial class ConnectionForm : Form
     {
+        string serverIP;
+        string serverPort;
         public ConnectionForm()
         {
             InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConnectionForm_Load(object sender, EventArgs e)
+        {
+            serverIP = ConfigurationManager.AppSettings.Get("IP");
+            serverPort = ConfigurationManager.AppSettings.Get("Port");
+            IPLabel.Text = "Server IP address set to " + serverIP;
+            portLabel.Text = "Server port set to " + serverPort;
+            instructionLabel.Text = "To change the values, edit the App.config file and restart the program";
         }
     }
 }
