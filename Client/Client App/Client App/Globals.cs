84e66fdee1bf204d3cce70479649402278f76e4c
Mohanad <mohanadismail789@gmail.com>
2022-06-19 19:51:32 +0200

    Add global user reference

--- Client/Client App/Client App/Globals.cs
+++ Client/Client App/Client App/Globals.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Client_App.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Client_App
 {
     internal static class Globals
     {
+        static User user;
     }
 }
