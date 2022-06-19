f99f0ba6776adc4a95660406dc64d9b74ae77d73
Nazeeh Maged <nazeehmaged007@gmail.com>
2022-06-19 22:57:01 +0200

    Added back/deposit buttons functionality

--- Client/Client App/Client App/DepositForm.Designer.cs
+++ Client/Client App/Client App/DepositForm.Designer.cs
@@ -31,44 +31,91 @@
 			this.deposit = new System.Windows.Forms.Label();
 			this.depositButton = new System.Windows.Forms.Button();
 			this.depositTextbox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.currentBalance = new System.Windows.Forms.Label();
+			this.backButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// deposit
 			// 
 			this.deposit.AutoSize = true;
 			this.deposit.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.deposit.Location = new System.Drawing.Point(166, 98);
+			this.deposit.Location = new System.Drawing.Point(143, 137);
 			this.deposit.Name = "deposit";
-			this.deposit.Size = new System.Drawing.Size(55, 23);
+			this.deposit.Size = new System.Drawing.Size(156, 23);
 			this.deposit.TabIndex = 0;
-			this.deposit.Text = "label1";
+			this.deposit.Text = "Enter deposit value";
 			this.deposit.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// depositButton
 			// 
 			this.depositButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.depositButton.Location = new System.Drawing.Point(245, 181);
+			this.depositButton.Location = new System.Drawing.Point(279, 193);
 			this.depositButton.Name = "depositButton";
 			this.depositButton.Size = new System.Drawing.Size(94, 29);
 			this.depositButton.TabIndex = 1;
-			this.depositButton.Text = "button1";
+			this.depositButton.Text = "Deposit";
 			this.depositButton.UseVisualStyleBackColor = true;
 			this.depositButton.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// depositTextbox
 			// 
 			this.depositTextbox.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.depositTextbox.Location = new System.Drawing.Point(330, 98);
+			this.depositTextbox.Location = new System.Drawing.Point(361, 130);
 			this.depositTextbox.Name = "depositTextbox";
 			this.depositTextbox.Size = new System.Drawing.Size(125, 30);
 			this.depositTextbox.TabIndex = 2;
 			this.depositTextbox.TextChanged += new System.EventHandler(this.depositTextbox_TextChanged);
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(189, 140);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 20);
+			this.label1.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label2.Location = new System.Drawing.Point(143, 73);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(132, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Current balance";
+			// 
+			// currentBalance
+			// 
+			this.currentBalance.AutoSize = true;
+			this.currentBalance.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.currentBalance.Location = new System.Drawing.Point(361, 73);
+			this.currentBalance.Name = "currentBalance";
+			this.currentBalance.Size = new System.Drawing.Size(132, 23);
+			this.currentBalance.TabIndex = 5;
+			this.currentBalance.Text = "Current balance";
+			this.currentBalance.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// backButton
+			// 
+			this.backButton.Location = new System.Drawing.Point(12, 12);
+			this.backButton.Name = "backButton";
+			this.backButton.Size = new System.Drawing.Size(64, 30);
+			this.backButton.TabIndex = 6;
+			this.backButton.Text = "Back";
+			this.backButton.UseVisualStyleBackColor = true;
+			this.backButton.Click += new System.EventHandler(this.backButton_Click);
+			// 
 			// DepositForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(611, 291);
+			this.ClientSize = new System.Drawing.Size(695, 254);
+			this.Controls.Add(this.backButton);
+			this.Controls.Add(this.currentBalance);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.depositTextbox);
 			this.Controls.Add(this.depositButton);
 			this.Controls.Add(this.deposit);
@@ -85,5 +132,9 @@
 		private Label deposit;
 		private Button depositButton;
 		private TextBox depositTextbox;
+		private Label label1;
+		private Label label2;
+		private Label currentBalance;
+		private Button backButton;
 	}
 }
--- Client/Client App/Client App/DepositForm.cs
+++ Client/Client App/Client App/DepositForm.cs
@@ -4,21 +4,29 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Client_App.Classes;
+using static Client_App.Globals;
 
 namespace Client_App
 {
     public partial class DepositForm : Form
     {
-        public DepositForm()
+		private Form backForm;
+        public DepositForm(Form backForm)
         {
             InitializeComponent();
+			this.backForm = backForm;
+
         }
 
 		private void DepositForm_Load(object sender, EventArgs e)
 		{
+			//currentBalance.Text = user.balance;
 
 		}
 
@@ -30,8 +38,29 @@ namespace Client_App
 		private void button1_Click(object sender, EventArgs e)
 		{
 			float depositValue;
-			if (float.TryParse(depositTextbox.Text, out depositValue))
+			if (float.TryParse(depositTextbox.Text, out depositValue))	  
 			{
+				if (depositValue > 0)
+				{
+					DepositRequest depositRequest = new DepositRequest(depositValue);
+					string serialRequestString = JsonSerializer.Serialize(depositRequest);//changed to string
+
+					NetworkStream serverStream = clientSocket.GetStream();
+					byte[] outStream = System.Text.Encoding.ASCII.GetBytes(serialRequestString);//changed to bytes 
+					serverStream.Write(outStream, 0, outStream.Length);//send to socket
+					serverStream.Flush();
+
+					byte[] inStream = new byte[10025];
+					serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+					user.balance = float.Parse(System.Text.Encoding.ASCII.GetString(inStream));
+
+					currentBalance.Text = user.balance;	   
+				}
+				else 
+				{
+					MessageBox.Show("Please enter a postive value", "Negative value ", MessageBoxButtons.OK);
+				}
+
 			}
 			else {
 				MessageBox.Show("Please enter a float value", "Wrong datatype",MessageBoxButtons.OK );
@@ -43,5 +72,16 @@ namespace Client_App
 		{
 
 		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void backButton_Click(object sender, EventArgs e) 
+		{ 
+			this.Close();
+			backForm.Show();
+		}	
 	}
 }
