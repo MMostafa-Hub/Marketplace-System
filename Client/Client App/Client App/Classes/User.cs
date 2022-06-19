ab7df253acfd58fa69b6144e9c2266fcc615e58a
Mohanad <mohanadismail789@gmail.com>
2022-06-19 18:00:06 +0200

    Add Cart and User classes

---
+++ Client/Client App/Client App/Classes/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_App.Classes
+{
+    internal class User
+    {
+        public string username;
+        public float balance;
+        public Cart cart;
+    }
+}
