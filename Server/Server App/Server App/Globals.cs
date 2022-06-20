8fc8de085613fea2d7c1834660dcb4eafda3d618
Mohanad <mohanadismail789@gmail.com>
2022-06-20 13:56:04 +0200

    Add global reference to database connection

--- Server/Server App/Server App/Globals.cs
+++ Server/Server App/Server App/Globals.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace Server_App
 {
     internal static class Globals
     {
+        private static SqlConnection? dbConnection = null;
+
+        public static SqlConnection getDBConnection()
+        {
+            if (dbConnection == null)
+            {
+                string connectionString = "Data Source = (LocalDb)\\Project; Initial Catalog = Parallel; Integrated Security = True";
+                dbConnection = new SqlConnection(connectionString);
+                dbConnection.Open();
+            }
+            return dbConnection;
+        }
     }
 }
