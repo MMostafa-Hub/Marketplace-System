27db22a658d5e526fa99d8dbbfc2dba63d59aba3
Hazem Hisham <hazemhesham10920@gmail.com>
2022-06-20 21:23:56 +0200

    Added implementation for add to cart

--- Server/Server App/Server App/Functions/Add_Cart.cs
+++ Server/Server App/Server App/Functions/Add_Cart.cs
@@ -11,12 +11,30 @@ namespace Server_App
 {
     internal static class Add_Cart
     {
-        public static string addToCart(addToCartRequest request, string username)
+        /*Function_Name:addToCart
+        *Input:AddToCartRequest ,username
+        *Ouput:Inserts users selection into cart with the quantity specified in request(inserts into table has_in_cart)
+        *Function Type:Non-Synchronized
+        */
+        public static void addToCart(addToCartRequest request, string username)
         { 
-            SqlConnection cn=Globals.getDBConnection();
-            SqlDataAdapter adapter =new SqlDataAdapter();
-            string sql;
-            return "";
+            /*Establishing Connection with Database*/
+            SqlConnection connection=Globals.getDBConnection();
+            /*Creation of adapter used to execute insert update delete commands*/
+            SqlDataAdapter adapter = new SqlDataAdapter(); 
+            int ID=request.productID;
+            int Quantity=request.quantity;
+            /*Creation of string that contains The query*/
+            string sql="insert into has_in_cart Values('"+username+"',"+ID.ToString()+","+Quantity.ToString()+")";
+            /*Creating The command that will be executed*/
+            SqlCommand command = new SqlCommand(sql, connection);
+            /*Exectuion of Insertion int has_in_cart Table containing user_username,productid and quantity*/
+            adapter.InsertCommand = new SqlCommand(sql, connection);
+            adapter.InsertCommand.ExecuteNonQuery();
+            /*closing connection and disposing command for Performance*/
+            command.Dispose();
+            connection.Close();
+            
         }
        
     }
