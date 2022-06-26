6a6b4978f83fc9d0ab6883fe221ab251517b7661
MariaOsama2 <85039110+MariaOsama2@users.noreply.github.com>
2022-06-26 13:25:06 +0200

    Add files via upload
    Add Dashboard form

---
+++ Client/Client App/Client App/DashboardForm.cs
@@ -0,0 +1,14 @@
+namespace Client_App
+{
+    public partial class DashboardForm : Form
+    {
+        public DashboardForm()
+        {
+            InitializeComponent();
+            CreateDashboard();
+        }
+
+        public void CreateDashboard()
+        {}
+    }
+}
