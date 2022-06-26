82e9c2e092993c04923e73587d65f700b61e8fa4
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 14:43:38 +0200

    Add AdminLoginResponse class

---
+++ Server/Server App/Server App/Classes/AdminLoginResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class AdminLoginResponse : AbstractResponse
+    {
+        bool success;
+
+        public AdminLoginResponse(bool success)
+        {
+            this.success = success;
+        }
+    }
+}
