23a73ca8d4fc641f045817cc28555756ab5e6315
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 11:39:28 +0200

    Add Order Class

---
+++ Server/Server App/Server App/Classes/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class Order
+    {
+        public string dateCreated;
+        public List<Tuple<string, int>> productsInOrder;
+
+        public Order(string dateCreated, List<Tuple<string, int>> productsInOrder)
+        {
+            this.dateCreated = dateCreated;
+            this.productsInOrder = productsInOrder;
+        }
+    }
+}
