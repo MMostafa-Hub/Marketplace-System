927e64f266d6584beb96c0c8ef6a56ebc330a88e
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 14:37:43 +0200

    Add UserLoginRequest class

---
+++ Server/Server App/Server App/Classes/UserLoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class UserLoginRequest :Request
+    {
+        String username;
+        String password;
+
+        public UserLoginRequest(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+
+            type = "userLogin";
+        }
+    }
+}
