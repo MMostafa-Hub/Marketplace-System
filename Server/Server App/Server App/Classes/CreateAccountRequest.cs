926278e422f34e870f8ff401d83378f98d8d7795
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 14:30:54 +0200

    Add CreateAccountRequest class

---
+++ Server/Server App/Server App/Classes/CreateAccountRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes 
+{
+    internal class CreateAccountRequest : Request
+    {
+        String username;
+        String password;
+        String gender;
+        int age;
+
+        public CreateAccountRequest(string username, string password, string gender, int age)
+        {
+            this.username = username;
+            this.password = password;
+            this.gender = gender;
+            this.age = age;
+
+            type = "createAccount";
+        }
+    }
+}
