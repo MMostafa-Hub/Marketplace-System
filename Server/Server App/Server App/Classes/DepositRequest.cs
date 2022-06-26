1a3222353070db4b456dbe459495f969e146a383
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 11:27:28 +0200

    Add DepositRequest Class

---
+++ Server/Server App/Server App/Classes/DepositRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class DepositRequest : Request
+    {
+        public float depositValue;
+        public DepositRequest(float depValue)
+        {
+            depositValue = depValue;
+            type = "deposit";
+        }
+    }
+}
