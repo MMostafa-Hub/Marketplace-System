862eef8a84ab5f08378a17ee57e0f69d52bdcd5e
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 13:29:27 +0200

    Add Deposit Function Needs testing

---
+++ Server/Server App/Server App/Functions/Deposit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Server_App.Classes;
+
+namespace Server_App.Functions
+{
+    internal static class Deposit
+    {
+        public static void depositBalance(DepositRequest depositRequest)
+        {
+            SqlConnection connection = Globals.getDBConnection();
+
+            
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql;
+
+            float newBalance = depositRequest.depositValue;
+
+            sql = "UPDATE users SET balance = " + newBalance.ToString() + " WHERE username= "  + '"'+ Globals.client_username + '"';
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            //adapter.InsertCommand = new SqlCommand(sql, connection);
+            adapter.InsertCommand = command;
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            command.Dispose();
+        }
+    }
+}
