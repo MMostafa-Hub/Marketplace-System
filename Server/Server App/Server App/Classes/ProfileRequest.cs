e9b75f080a36ffa94fc54a1ce6dea6ee9d1d63fa
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 11:40:19 +0200

    Add ProfileRequest Class

---
+++ Server/Server App/Server App/Classes/ProfileRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class ProfileRequest : Request
+    {
+        public string profileUserName;
+        public ProfileRequest(string profileUserName)
+        {
+            this.profileUserName = profileUserName;
+            type = "profile";
+        }
+    }
+}
