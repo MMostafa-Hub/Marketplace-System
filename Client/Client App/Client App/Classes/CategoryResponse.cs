8254b044bc774ca1bdfb21da13068722b195b696
Nazeeh Maged <nazeehmaged007@gmail.com>
2022-06-26 11:09:09 +0200

    Added class CategoryReponse

---
+++ Client/Client App/Client App/Classes/CategoryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_App.Classes
+{
+	internal class CategoryResponse
+	{
+		public List<string>categoryList=new List<string>();
+		public List<Product>productList=	new List<Product>();
+	}
+}
