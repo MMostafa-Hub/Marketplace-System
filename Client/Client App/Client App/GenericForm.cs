0c865297f3f2ce603c09641dff6e8abba328621a
Mohanad <mohanadismail789@gmail.com>
2022-06-26 20:10:54 +0200

    Create GenericForm

---
+++ Client/Client App/Client App/GenericForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_App
+{
+    internal class GenericForm : Form
+    {
+        protected override void OnFormClosing(System.Windows.Forms.FormClosingEventArgs e)
+        {
+            //TODO: send logout request
+            Application.Exit();
+        }
+    }
+}
