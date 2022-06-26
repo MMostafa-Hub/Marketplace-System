4a66e4d30b48af37aa5fe97b0bf1e046fd929ad7
Nazeeh Maged <nazeehmaged007@gmail.com>
2022-06-26 11:09:09 +0200

    Added class CategoryReponse

--- Client/Client App/Client App/SearchForm.cs
+++ Client/Client App/Client App/SearchForm.cs
@@ -1,28 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿using Client_App.Classes;
 namespace Client_App
 {
-    public partial class SearchForm : Form
-    {
-        private Form backForm;
-        public SearchForm(Form backForm)
-        {
-            InitializeComponent();
-            this.backForm = backForm;   
-        }
+	public partial class SearchForm : Form
+	{
+		private Form backForm;
+		public SearchForm(Form backForm)
+		{
+			InitializeComponent();
+			this.backForm = backForm;
+		}
 
 		private void btn_back_Click(object sender, EventArgs e)
 		{
-            this.Close();
-            backForm.Show();
-        }
+			this.Close();
+			backForm.Show();
+		}
+
+		private void categoryLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void SearchForm_Load(object sender, EventArgs e)
+		{
+			Request categoryRequest = new Request();
+			categoryRequest.type = "CategoryRequest";
+			clientSocket.write(categoryRequest);
+
+			CategoryResponse cateResponse = clientSocket.read<CategoryResponse>(timeout:100);
+			foreach (string category in cateResponse.categoryList)
+			{
+				categorycombobox.Add(category);
+			}
+
+			foreach (Product product in cateResponse.productList) 
+			{
+				string[] row = { product.name.ToString(), product.price.ToString(), product.stockQuantity.ToString() };
+				dataGridView1.Rows.Add(row);
+			}
+
+			//DATAVIEW ITEMS
+
+			//List<Product>recevieditems=//recevieditem list
+			for (int i = 0; i < recevieditems.Size(); i++)
+			{
+				string[] row = { recevieditems[i].name.ToString(), recevieditems[i].price.ToString(), recevieditems[i].stockQuantity.ToString() };
+				dataGridView1.Rows.Add(row);
+			}
+
+			List<string> sortBy = new List<string>();
+			sortBy.Add("Price");
+			sortBy.Add("Latest product");
+			sortcombobox.DataSource = sortBy;
+
+		}
+
+		private void sortcombobox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void searchTxtBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btn_search_Click(object sender, EventArgs e)
+		{
+			string searchText = searchTxtBox.Text;
+			string categoryText = categorycombobox.SelectedItem.ToString();
+			string sortText = sortcombobox.SelectedItem.ToString();
+			SearchRequest srequest = new SearchRequest(searchText, categoryText, sortText);
+
+
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
 	}
 }
