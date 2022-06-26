8b969a79557d3a63dac2bf93bb25849d31677774
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 14:42:50 +0200

    Add AdminLoginRequest class

---
+++ Server/Server App/Server App/Classes/AdminLoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class AdminLoginRequest : Request
+    {
+        String username;
+        String password;
+
+        public AdminLoginRequest(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+
+            type = "adminLogin";
+        }
+    }
+}
