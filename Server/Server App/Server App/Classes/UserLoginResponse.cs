1e87dae095c43914d34d8dc08b3de8190a4bb1b2
Mario Kamel <mario.medhat99@gmail.com>
2022-06-26 14:38:33 +0200

    Add UserLoginResponse class

---
+++ Server/Server App/Server App/Classes/UserLoginResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class UserLoginResponse : AbstractResponse
+    {
+        bool success;
+
+        public UserLoginResponse(bool success)
+        {
+            this.success = success;
+        }
+    }
+}
