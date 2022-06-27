0772140a415fe4daac7560c85913b11dc616c683
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2022-06-27 14:24:04 +0200

    modify categorySearch to return all product info

--- Server/Server App/Server App/Functions/Search.cs
+++ Server/Server App/Server App/Functions/Search.cs
@@ -37,22 +37,27 @@ namespace Server_App.Functions
 
             dataReader.Close();
 
-            sql = "SELECT id, name, price, stockQuantity FROM Product";
+            sql = "SELECT * FROM Product";
             command = new SqlCommand(sql, sqlConnection);
 
             dataReader = command.ExecuteReader();
 
             while (dataReader.Read())
             {
-                int id = int.Parse(dataReader.GetString(0));
+                int id = (int)dataReader.GetValue(0);
                 string name = dataReader.GetString(1);
-                float price = dataReader.GetFloat(2);
-                int stock = int.Parse(dataReader.GetString(3));
-                products.Add(new Product( id, name, price, stock));
+                string description = dataReader.GetString(2);
+                string category = dataReader.GetString(3);
+                float price = ((float)(double)dataReader.GetValue(4));
+                int stock = (int) dataReader.GetValue(5);
+                int sold = (int)dataReader.GetValue(6);
+                products.Add(new Product( id, name,description,category, price, stock,sold));
             }
             CategoryResponse response = new CategoryResponse(categories, products);
 
             return response;
         }
+
+        
     }
 }
