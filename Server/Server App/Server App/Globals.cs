53bea3ae2880045b15631aaa7a2864292548bc96
Hazem Hisham <hazemhesham10920@gmail.com>
2022-06-27 23:25:43 +0200

    Removed username from globals

--- Server/Server App/Server App/Globals.cs
+++ Server/Server App/Server App/Globals.cs
@@ -11,8 +11,7 @@ namespace Server_App
     {
         /* Client Username shared in the thread address space only */
         [ThreadStatic]
-        public static string client_username;
-
+       
         private static SqlConnection? dbConnection = null;
 
         public static SqlConnection getDBConnection()
