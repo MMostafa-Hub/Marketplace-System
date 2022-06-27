e3aeaa8252069cd7478d53d574fd80be8440ca65
Mamdouh Ibrahim Youssef Mohamed <mamdouhmekky123@gmail.com>
2022-06-27 18:57:23 +0200

    Update product Form

---
+++ Client/Client App/Client App/ProductForm2.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Client_App
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ProductForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ProductForm";
+            this.Load += new System.EventHandler(this.ProductForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label LB1;
+        private Label LB2;
+        private Label LB4;
+        private Label LB5;
+        private Label LB6;
+        private Label LB3;
+        private Label LB2_2;
+        private TextBox TB1;
+        private Label LB3_3;
+        private Label LB4_4;
+        private Button BT1;
+        private Button BT2;
+        private Label LB5_5;
+    }
+}
---
+++ Client/Client App/Client App/ProductForm2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_App
+{
+    public partial class ProductForm : Form
+    {
+       Product pro;
+        Form ProForm;
+
+        public ProductForm(Product  pro, Form ProForm)
+        {
+            InitializeComponent();
+            this.pro = pro;
+            this.ProForm = ProForm;
+        }
+
+
+        private void BT2_Click(object sender, EventArgs e)
+        {
+            HomeForm hf = new HomeForm(this);
+            this.Close();
+            hf.Show();
+
+        }
+
+        private void ProductForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BT1_Click(object sender, EventArgs e)
+         {
+             LB2_2.Text = this.pro.id;
+             LB3_3.Text = this.pro.name;
+
+            //TB1.Text = this.pro.Quantity;
+
+             LB4_4.Text = this.pro.price;
+             LB5_5.Text = this.pro.stockQuantity;
+
+
+
+         }
+
+    }
+}
