24d31a62b31aba659aa3756e4c7bf2a9124ca638
Hazem Hisham <hazemhesham10920@gmail.com>
2022-06-27 18:07:42 +0200

    add balance deduction

--- Server/Server App/Server App/Functions/Checkout.cs
+++ Server/Server App/Server App/Functions/Checkout.cs
@@ -12,21 +12,37 @@ namespace Server_App
     {
         public static int checkOut(checkOutRequest request,float totalAmount,string date)
         {
-          /*Establishing Connection with Database*/
-          SqlConnection connection=Globals.getDBConnection();
-          /*Stateful server has username*/
-          string username=Globals.client_username;
-          string sql ="";
-          /*Retrieval command utilities*/
-          SqlCommand command;
-          SqlDataReader dataReader;
-          sql = "SELECT balance FROM users where username='" + username + "'";
-          command = new SqlCommand(sql, connection);
+            /*Establishing Connection with Database*/
+            SqlConnection connection=Globals.getDBConnection();
+            /*Stateful server has username*/
+            string username=Globals.client_username;
+            string sql ="";
+            /*Retrieval command utilities*/
+            SqlCommand command;
+            SqlDataReader dataReader;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            List<int>products=new List<int>();
+            sql = "SELECT balance FROM users where username='" + username + "'";
+            command = new SqlCommand(sql, connection);
 
-          dataReader = command.ExecuteReader();
-       
-          
-          
+            dataReader = command.ExecuteReader();
+            float balance=0;
+            while (dataReader.Read())
+            {
+              balance = (float)(double)dataReader.GetValue(0);
+            }
+            dataReader.Close();
+            command.Dispose();
+            /*1-Handling users table,we should deduct total amount from user balance(Updating user balance with new amount)*/
+            balance-=totalAmount;
+            sql = "UPDATE users SET balance = " + totalAmount.ToString() + " WHERE username= "  + "'" +username + "'";
+            command = new SqlCommand(sql, connection);
+            adapter.InsertCommand = command;
+            adapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+
+
+            /**/
 
 
             return 0;
