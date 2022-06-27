5aa24195604701663615d08acdb6752c1409f016
Mario Kamel <mario.medhat99@gmail.com>
2022-06-27 23:36:41 +0200

    Add logout Function

---
+++ Server/Server App/Server App/Classes/LogoutRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_App.Classes
+{
+    internal class LogoutRequest:Request
+    {
+        public LogoutRequest()
+        {
+            type = "logout";
+        }
+    }
+}
