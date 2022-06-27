e55a10934b96df505c0854b93bc48ab83cc4816c
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2022-06-27 21:51:02 +0200

    add search method

--- Server/Server App/Server App/Functions/Search.cs
+++ Server/Server App/Server App/Functions/Search.cs
@@ -10,6 +10,28 @@ namespace Server_App.Functions
 {
     internal class Search
     {
+        static List<Product>  addProductsToList(SqlDataReader dataReader)
+        {
+            List<Product> products = new List<Product>();
+            
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    int id = dataReader.GetInt32(0);
+                    string name = dataReader.GetString(1);
+                    string description = dataReader.GetString(2);
+                    string categories = dataReader.GetString(3);
+                    float price = ((float)(double)dataReader.GetValue(4));
+                    int stock = dataReader.GetInt32(5);
+                    int sold = dataReader.GetInt32(6);
+                    products.Add(new Product(id, name, description, categories, price, stock, sold));
+                }
+            }
+
+            
+            return products;
+        }
         static CategoryResponse categorySearch()
         {
 
@@ -18,7 +40,7 @@ namespace Server_App.Functions
 
             SqlCommand command;
             SqlDataReader dataReader;
-            String sql, output = "";
+            String sql;
 
             List<string> categories = new List<string>();
             List<Product> products = new List<Product>();
@@ -42,22 +64,65 @@ namespace Server_App.Functions
 
             dataReader = command.ExecuteReader();
 
-            while (dataReader.Read())
-            {
-                int id = (int)dataReader.GetValue(0);
-                string name = dataReader.GetString(1);
-                string description = dataReader.GetString(2);
-                string category = dataReader.GetString(3);
-                float price = ((float)(double)dataReader.GetValue(4));
-                int stock = (int) dataReader.GetValue(5);
-                int sold = (int)dataReader.GetValue(6);
-                products.Add(new Product( id, name,description,category, price, stock,sold));
-            }
+            products = addProductsToList(dataReader);
+
+            dataReader.Close();
+
             CategoryResponse response = new CategoryResponse(categories, products);
 
             return response;
         }
 
-        
+        static SearchResponse search(SearchRequest request)
+        {
+
+            List<Product> products = new List<Product>();
+            SqlConnection sqlConnection = Globals.getDBConnection();
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql;
+
+            string? search = request.searchtxt, category = request.categorytxt;
+            string sortType = request.sorttxt; 
+
+           // sql = "SELECT DISTINCT category FROM Product";
+           // command = new SqlCommand(sql, sqlConnection);
+
+            if (search != null && category != null)
+            {
+                //to be updated desc or asc
+                sql = "SELECT * FROM Product WHERE (category = '" + category + "' AND name LIKE '%" + search + "%') ORDER BY " + sortType;
+                command = new SqlCommand(sql, sqlConnection);
+
+                dataReader = command.ExecuteReader();
+                products = addProductsToList(dataReader);
+
+                dataReader.Close();
+            }
+            else if (search == null && category != null)
+            {
+                sql = "SELECT * FROM Product WHERE category = '" + category + "'  ORDER BY " + sortType;
+                command = new SqlCommand(sql, sqlConnection);
+
+                dataReader = command.ExecuteReader();
+                products = addProductsToList(dataReader);
+
+                dataReader.Close();
+            }
+            else if (search != null && category == null)
+            {
+                sql = "SELECT * FROM Product WHERE (name LIKE '%" + search + "%') ORDER BY " + sortType;
+                command = new SqlCommand(sql, sqlConnection);
+
+                dataReader = command.ExecuteReader();
+                products = addProductsToList(dataReader);
+
+                dataReader.Close();
+            }
+
+            SearchResponse response = new SearchResponse(products);
+            return response;
+        }
+
     }
 }
