c47b18b3fcee764f2ae3ed4d444dd7db572202be
MariaOsama2 <1809094@eng.asu.edu.eg>
2022-06-28 03:58:47 +0200

    Add ProfileForm

---
+++ Client/Client App/Client App/ProfileForm.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Client_App
+{
+    partial class ProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonDeposit = new System.Windows.Forms.Button();
+            this.labelUserBalance = new System.Windows.Forms.Label();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.UserPictureBox = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridViewOH = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateCreated = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProdName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProdQuantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelOrdHisto = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UserPictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOH)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.buttonDeposit);
+            this.panel1.Controls.Add(this.labelUserBalance);
+            this.panel1.Controls.Add(this.labelUserName);
+            this.panel1.Controls.Add(this.labelBalance);
+            this.panel1.Controls.Add(this.UserPictureBox);
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 97);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonDeposit
+            // 
+            this.buttonDeposit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonDeposit.Location = new System.Drawing.Point(424, 66);
+            this.buttonDeposit.Name = "buttonDeposit";
+            this.buttonDeposit.Size = new System.Drawing.Size(67, 24);
+            this.buttonDeposit.TabIndex = 5;
+            this.buttonDeposit.Text = "Deposit";
+            this.buttonDeposit.UseVisualStyleBackColor = true;
+            this.buttonDeposit.Click += new System.EventHandler(this.buttonDeposit_Click);
+            // 
+            // labelUserBalance
+            // 
+            this.labelUserBalance.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelUserBalance.AutoSize = true;
+            this.labelUserBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelUserBalance.Location = new System.Drawing.Point(327, 65);
+            this.labelUserBalance.Name = "labelUserBalance";
+            this.labelUserBalance.Size = new System.Drawing.Size(65, 24);
+            this.labelUserBalance.TabIndex = 4;
+            this.labelUserBalance.Text = "$$$$$";
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelUserName.Location = new System.Drawing.Point(327, 27);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(114, 24);
+            this.labelUserName.TabIndex = 3;
+            this.labelUserName.Text = "User Name";
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelBalance.Location = new System.Drawing.Point(230, 65);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(91, 24);
+            this.labelBalance.TabIndex = 2;
+            this.labelBalance.Text = "Balance:";
+            // 
+            // UserPictureBox
+            // 
+            this.UserPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UserPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.UserPictureBox.Image = global::Client_App.Properties.Resources.user;
+            this.UserPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.UserPictureBox.Name = "UserPictureBox";
+            this.UserPictureBox.Size = new System.Drawing.Size(194, 95);
+            this.UserPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UserPictureBox.TabIndex = 1;
+            this.UserPictureBox.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelName.Location = new System.Drawing.Point(230, 27);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(71, 24);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(504, 412);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridViewOH);
+            this.panel2.Controls.Add(this.labelOrdHisto);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 147);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(498, 262);
+            this.panel2.TabIndex = 2;
+            // 
+            // dataGridViewOH
+            // 
+            this.dataGridViewOH.AllowUserToAddRows = false;
+            this.dataGridViewOH.AllowUserToDeleteRows = false;
+            this.dataGridViewOH.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewOH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOH.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.DateCreated,
+            this.ProdName,
+            this.ProdQuantity});
+            this.dataGridViewOH.Location = new System.Drawing.Point(9, 67);
+            this.dataGridViewOH.Name = "dataGridViewOH";
+            this.dataGridViewOH.ReadOnly = true;
+            this.dataGridViewOH.RowTemplate.Height = 25;
+            this.dataGridViewOH.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewOH.Size = new System.Drawing.Size(476, 186);
+            this.dataGridViewOH.TabIndex = 2;
+            // 
+            // Index
+            // 
+            this.Index.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Index.HeaderText = "Order Number";
+            this.Index.Name = "Index";
+            this.Index.ReadOnly = true;
+            // 
+            // DateCreated
+            // 
+            this.DateCreated.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DateCreated.HeaderText = "Date";
+            this.DateCreated.Name = "DateCreated";
+            this.DateCreated.ReadOnly = true;
+            // 
+            // ProdName
+            // 
+            this.ProdName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ProdName.HeaderText = "Product Name";
+            this.ProdName.Name = "ProdName";
+            this.ProdName.ReadOnly = true;
+            // 
+            // ProdQuantity
+            // 
+            this.ProdQuantity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ProdQuantity.HeaderText = "Quantity";
+            this.ProdQuantity.Name = "ProdQuantity";
+            this.ProdQuantity.ReadOnly = true;
+            // 
+            // labelOrdHisto
+            // 
+            this.labelOrdHisto.AutoSize = true;
+            this.labelOrdHisto.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelOrdHisto.Location = new System.Drawing.Point(9, 23);
+            this.labelOrdHisto.Name = "labelOrdHisto";
+            this.labelOrdHisto.Size = new System.Drawing.Size(182, 29);
+            this.labelOrdHisto.TabIndex = 1;
+            this.labelOrdHisto.Text = "Orders History";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.buttonLogout);
+            this.panel3.Controls.Add(this.buttonBack);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(498, 35);
+            this.panel3.TabIndex = 3;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonLogout.Location = new System.Drawing.Point(408, 0);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(90, 35);
+            this.buttonLogout.TabIndex = 1;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonBack.Location = new System.Drawing.Point(0, 0);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(90, 35);
+            this.buttonBack.TabIndex = 0;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // ProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 412);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(520, 450);
+            this.Name = "ProfileForm";
+            this.Text = "Profile";
+            this.Load += new System.EventHandler(this.ProfileForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UserPictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOH)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox UserPictureBox;
+        private Label labelName;
+        private Panel panel2;
+        private Button buttonDeposit;
+        private Label labelUserBalance;
+        private Label labelUserName;
+        private Label labelBalance;
+        private DataGridView dataGridViewOH;
+        private DataGridViewTextBoxColumn Index;
+        private DataGridViewTextBoxColumn DateCreated;
+        private DataGridViewTextBoxColumn ProdName;
+        private DataGridViewTextBoxColumn ProdQuantity;
+        private Label labelOrdHisto;
+        private Panel panel3;
+        private Button buttonLogout;
+        private Button buttonBack;
+    }
+}
---
+++ Client/Client App/Client App/ProfileForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Client_App.Classes;
+using static Client_App.Globals;
+
+namespace Client_App
+{
+    public partial class ProfileForm : GenericForm
+    {
+        Form backForm, logoutForm;
+        private Label labelMsg = null;
+
+        public ProfileForm(Form backForm, Form logoutForm)
+        {
+            InitializeComponent();
+            this.backForm = backForm;
+            this.logoutForm = logoutForm;
+            labelUserName.Text = user.username;
+            labelUserBalance.Text = user.balance.ToString();
+        }
+
+        private void ProfileForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket.write(new ProfileRequest());
+            }
+            catch (Exception)
+            {
+                returnForm = this;
+                connectionForm.Show();
+                this.Hide();
+            }
+
+            ProfileResponse profileResponse = null;
+            try
+            {
+                profileResponse = clientSocket.read<ProfileResponse>(timeout: 100);
+            }
+            catch (Exception)
+            {
+                returnForm = this;
+                connectionForm.Show();
+                this.Hide();
+            }
+            if (profileResponse != null)
+            {
+                if (profileResponse.ordersHistory.Count == 0)
+                {
+                    //dataGridViewOH.RowHeadersVisible = false;
+                    dataGridViewOH.Visible = false;
+                    labelMsg = new Label();
+                    labelMsg.Name = "labelMsg";
+                    labelMsg.Anchor = AnchorStyles.None;
+                    labelMsg.AutoSize = true;
+                    labelMsg.Text = "No Orders yet!";
+                    labelMsg.Location = new Point(10, 67);
+                    labelMsg.Font = new Font("Nirmala UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+                    this.Controls.Add(labelMsg);
+                }
+                else
+                {
+                    if (labelMsg != null)
+                    {
+                        this.Controls.Remove(labelMsg);
+                        labelMsg = null;
+                        //dataGridViewOH.RowHeadersVisible = true;
+                        dataGridViewOH.Visible = true;
+                    }
+                    int index = 1;
+                    foreach (Order order in profileResponse.ordersHistory)
+                    {
+                        string[] row = { index.ToString(), order.dateCreated, "", "" };
+                        dataGridViewOH.Rows.Add(row);
+                        foreach (Tuple<string, int> tuple in order.productsInOrder)
+                        {
+                            string[] innerRow = { "", "", tuple.Item1, tuple.Item2.ToString() };
+                            dataGridViewOH.Rows.Add(innerRow);
+                        }
+                        index++;
+                    }
+                }
+            }
+        }
+
+        private void buttonDeposit_Click(object sender, EventArgs e)
+        {
+            DepositForm depositForm = new DepositForm(this);
+            this.Hide();
+            depositForm.Show();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            backForm.Show();
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure you want to Logout?";
+            string title = "Confirm Logout";
+            MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+            {
+                Request logoutRequest = new Request("logout");
+                clientSocket.write(logoutRequest);
+                this.Hide();
+                logoutForm.Show();
+                MessageBox.Show("You have logged out successfully", "Logout");
+            }
+        }
+    }
+}
