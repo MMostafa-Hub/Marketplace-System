b453909835109f57b8cefdbc485b951a4220402b
Mohamed5510 <mohamed.m.i1551@gmail.com>
2022-06-28 17:51:30 +0200

    password encryption

--- Client/Client App/Client App/AdminLoginForm.Designer.cs
+++ Client/Client App/Client App/AdminLoginForm.Designer.cs
@@ -111,6 +111,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(295, 290);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(179, 27);
             this.textBox2.TabIndex = 7;
             // 
--- Client/Client App/Client App/UserLoginForm.Designer.cs
+++ Client/Client App/Client App/UserLoginForm.Designer.cs
@@ -72,6 +72,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(286, 311);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(223, 27);
             this.textBox2.TabIndex = 3;
             // 
