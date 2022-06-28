a4c78cc8d995aa702bb812796ed5c1af374a8093
Mohamed5510 <mohamed.m.i1551@gmail.com>
2022-06-28 14:41:30 +0200

    First Form is done

---
+++ Client/Client App/Client App/FirstForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Client_App
+{
+    partial class FirstForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Shrikhand", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(179, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 109);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fady Shop";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Admin Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "User Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(562, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Create Account";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FirstForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "FirstForm";
+            this.Text = "FirstForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
