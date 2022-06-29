997b178f2eea16b20579997e349fc888f53bdb91
Mohamed5510 <mohamed.m.i1551@gmail.com>
2022-06-29 15:49:55 +0200

    Back Buttons & error messages modification

--- Client/Client App/Client App/UserLoginForm.Designer.cs
+++ Client/Client App/Client App/UserLoginForm.Designer.cs
@@ -38,6 +38,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -124,12 +125,24 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "label5";
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(656, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 41);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "<---  Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // UserLoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Fuchsia;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -159,5 +172,6 @@
         private Label label3;
         private Label label4;
         private Label label5;
+        private Button button2;
     }
 }
