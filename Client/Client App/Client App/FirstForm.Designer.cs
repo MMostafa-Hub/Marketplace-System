51a4aee3f8680ceabc5b198d27b19841e0e6ec83
Mohamed5510 <mohamed.m.i1551@gmail.com>
2022-06-29 13:20:44 +0200

    full screen mode

--- Client/Client App/Client App/FirstForm.Designer.cs
+++ Client/Client App/Client App/FirstForm.Designer.cs
@@ -136,6 +136,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "FirstForm";
             this.Text = "FirstForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
