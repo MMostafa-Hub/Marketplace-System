155bc3a379b883d345cc35a1d7b98287aa593c22
Mohanad <mohanadismail789@gmail.com>
2022-06-29 16:34:43 +0200

    Add GenericForm2

---
+++ Client/Client App/Client App/GenericForm2.cs
@@ -0,0 +1,33 @@
+﻿using Client_App.Classes;
+using static Client_App.Globals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_App
+{
+    internal class GenericForm2 : Form
+    {
+        protected override void OnFormClosing(System.Windows.Forms.FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                Request logoutRequest = new Request("logout");
+                try
+                {
+                    clientSocket.write(logoutRequest);
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
